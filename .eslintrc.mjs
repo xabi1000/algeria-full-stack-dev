d45c53e887be41288e217e6254f6af023da14ffe
xabi1000 <babelwado@gmail.com>
2025-01-01 00:49:17 +0100

    feat: add new feature to improve user experience
    Added a new feature to the login page that allows users to reset their password.
    Updated the UI to make it more user-friendly.
    Fixed a bug that was causing the page to crash on certain browsers.
    
    Resolves #123

---
+++ .eslintrc.mjs
@@ -0,0 +1,31 @@
+export default {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:astro/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      rules: {},
+    },
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
