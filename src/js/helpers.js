63df1c00b841aecf6a3d2c9ea616f8c1b522c001
xabi1000 <babelwado@gmail.com>
2025-01-12 18:50:10 +0100

    feat: agregar archivo helpers.js con enlaces y iconos de redes sociales

---
+++ src/js/helpers.js
@@ -0,0 +1,15 @@
+const links = [
+  { text: 'Inicio', url: '/' },
+  { text: 'Servicios', url: '/servicios' },
+  { text: 'Portafolio', url: '/portafolio' },
+  { text: 'Contacto', url: '/contacto' },
+];
+
+const ICONS = [
+  { icon: "twitter" },
+  { icon: "facebook" },
+  { icon: "instagram" },
+  { icon: "linkedin" },
+]
+
+export { links, ICONS };
